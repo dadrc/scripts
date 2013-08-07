1dfd3ff1f4fdd31a5ec34882976c56e77853fd84
dadrc <double-oh-seven@web.de>
2013-08-07 09:07:06 +0200

    Update monitor-updates.sh

--- monitor-updates.sh
+++ monitor-updates.sh
@@ -1,3 +1,3 @@
 #!/bin/zsh
-array=("${(@f)$(apt-get -s dist-upgrade | grep -P "\d+ upgraded, \d+ newly installed, \d+ to remove and \d+ not upgraded."| grep -oP "[\d]+")}")
+array=("${(@f)$(apt-get -s dist-upgrade | grep -P "\d+ upgraded, \d+ newly installed, \d+ to remove and \d+ not upgraded."| grep -oP "\d+")}")
 echo "⬈ ${(j:/:)array} ·"
