7280984eb9a5e13f6355dc703f29915aaebbbb0b
drc <double-oh-seven@web.de>
2014-03-03 15:21:19 +0100

    Added encoding check to avoid some errors with older entries

--- kolab-getcalendarentry.py
+++ kolab-getcalendarentry.py
@@ -7,14 +7,13 @@ import sys
 def get_entries(files):
     for path in files:
         with open(path) as f:
-            msg = email.message_from_file(f)
+            mail = email.message_from_file(f)
             f.close()
-            print "-----\nCalendar entry from file {}\nID: {}\n-----".format(
-                path,
-                msg.get('Subject')
-            )
-            for part in msg.walk():
-                if part.get_content_subtype() == 'x-vnd.kolab.event':
+            for part in mail.walk():
+                if (part.get_content_subtype() == 'x-vnd.kolab.event'
+                        and part.get('Content-Transfer-Encoding') == 'base64'):
+                    msg = '-----\nCalendar entry from file {}\nID: {}\n-----'
+                    print msg.format(path, mail.get('Subject'))
                     print b64decode(part.get_payload())
 
 if __name__ == "__main__":
