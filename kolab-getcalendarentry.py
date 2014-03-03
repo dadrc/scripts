28791509b80e143249969802226f8c0f21c23855
drc <double-oh-seven@web.de>
2014-03-03 15:45:32 +0100

    Split up functionality into 2 functions

--- kolab-getcalendarentry.py
+++ kolab-getcalendarentry.py
@@ -4,17 +4,21 @@ import email
 import sys
 
 
-def get_entries(files):
-    msg = '-----\nCalendar entry from file {}\nID: {}\n-----'
+def get_mail(files):
     for path in files:
         with open(path) as f:
-            mail = email.message_from_file(f)
+            get_entry(path, email.message_from_file(f))
             f.close()
-            for part in mail.walk():
-                if (part.get_content_subtype() == 'x-vnd.kolab.event'
-                        and part.get('Content-Transfer-Encoding') == 'base64'):
-                    print msg.format(path, mail.get('Subject'))
-                    print b64decode(part.get_payload())
+
+
+def get_entry(path, mail):
+    msg = '-----\nCalendar entry from file {}\nID: {}\n-----'
+    for part in mail.walk():
+        if (part.get_content_subtype() == 'x-vnd.kolab.event'
+                and part.get('Content-Transfer-Encoding') == 'base64'):
+            print msg.format(path, mail.get('Subject'))
+            print b64decode(part.get_payload())
+
 
 if __name__ == "__main__":
-    get_entries(sys.argv[1:])
+    get_mail(sys.argv[1:])
